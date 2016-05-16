f145f5c66c2f33b31b5cb834fd9614fd8a390eed
Joerg Hohwiller <hohwille@users.sourceforge.net>
2016-05-16 09:27:08 +0200

    collected changes

--- mmm-service-api/src/main/java/net/sf/mmm/service/api/RemoteInvocationCallFailedException.java
+++ mmm-service-api/src/main/java/net/sf/mmm/service/api/RemoteInvocationCallFailedException.java
@@ -2,15 +2,15 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 package net.sf.mmm.service.api;
 
-import net.sf.mmm.util.NlsBundleUtilCoreRoot;
 import net.sf.mmm.util.exception.api.NlsRuntimeException;
+import net.sf.mmm.util.reflect.NlsBundleUtilReflectRoot;
 
 /**
  * This exception is thrown if {@link RemoteInvocationCall} has failed.
- * 
+ *
  * @see net.sf.mmm.service.api.client.RemoteInvocationCaller
  * @see net.sf.mmm.service.api.client.RemoteInvocationQueue#commit()
- * 
+ *
  * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
  * @since 1.0.0
  */
@@ -21,13 +21,13 @@ public class RemoteInvocationCallFailedException extends NlsRuntimeException {
 
   /**
    * The constructor.
-   * 
+   *
    * @param cause is the {@link Throwable} that caused this error.
    * @param operation is the {@link RemoteInvocationCall} that failed.
    */
   public RemoteInvocationCallFailedException(Throwable cause, RemoteInvocationCall operation) {
 
-    super(cause, createBundle(NlsBundleUtilCoreRoot.class).errorInvocationFailedOf(operation));
+    super(cause, createBundle(NlsBundleUtilReflectRoot.class).errorInvocationFailed(operation, null));
   }
 
 }
